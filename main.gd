ccc0b82d17fe6a130daa7b5f32869925d99dc774
Zachariah <49769704+zachariah-spencer@users.noreply.github.com>
2026-05-24 15:58:44 -0500

    Refactor main.gd for cleaner idiomatic GDScript

--- main.gd
+++ main.gd
@@ -1,19 +1,23 @@
 extends Node2D
 
 const DESIGN_SIZE := Vector2(1280, 720)
-const MAC_WINDOW_SCALE := 2.0
-
 const SPRITE_SIZE := 720.0
 const SPEED := 60.0
 const WATER_ALPHA := 0.5
+const WATER_TEXTURE := preload("res://art/water.png")
 
-func _ready():
+@onready var water_layer_right: Node2D = $WaterLayerRight
+@onready var water_layer_up: Node2D = $WaterLayerUp
+@onready var label: Label = $Label
+@onready var blue_square: Node2D = $BlueSquare
+
+func _ready() -> void:
 	setup_window_size()
 
-	setup_water_layer($WaterLayerRight)
-	setup_water_layer($WaterLayerUp)
+	setup_water_layer(water_layer_right)
+	setup_water_layer(water_layer_up)
 
-	$Label.text = "flow like water"
+	label.text = "flow like water"
 
 	update_water_positions(0.0)
 	update_label_position(0.0)
@@ -42,27 +46,27 @@ func center_window() -> void:
 
 func setup_water_layer(layer: Node2D) -> void:
 	for sprite in layer.get_children():
-		sprite.texture = preload("res://art/water.png")
+		sprite.texture = WATER_TEXTURE
 		sprite.scale = Vector2(
 			SPRITE_SIZE / sprite.texture.get_width(),
 			SPRITE_SIZE / sprite.texture.get_height()
 		)
 		sprite.modulate.a = WATER_ALPHA
 
-func _process(delta):
+func _process(_delta: float) -> void:
 	var t := Time.get_ticks_msec() / 1000.0
 
 	update_water_positions(t)
 	update_label_position(t)
 
-	$BlueSquare.position = get_global_mouse_position()
+	blue_square.position = get_global_mouse_position()
 
 func update_water_positions(t: float) -> void:
 	var screen_size := DESIGN_SIZE
 	var offset := fmod(t * SPEED, SPRITE_SIZE)
 
-	for i in range($WaterLayerRight.get_child_count()):
-		var sprite := $WaterLayerRight.get_child(i)
+	for i in range(water_layer_right.get_child_count()):
+		var sprite := water_layer_right.get_child(i)
 
 		sprite.position = Vector2(
 			-SPRITE_SIZE / 2.0 + i * SPRITE_SIZE + offset,
@@ -71,8 +75,8 @@ func update_water_positions(t: float) -> void:
 
 	var columns := int(ceil(screen_size.x / SPRITE_SIZE) + 2)
 
-	for i in range($WaterLayerUp.get_child_count()):
-		var sprite := $WaterLayerUp.get_child(i)
+	for i in range(water_layer_up.get_child_count()):
+		var sprite := water_layer_up.get_child(i)
 
 		var column := i % columns
 		var row := i / columns
@@ -84,12 +88,12 @@ func update_water_positions(t: float) -> void:
 
 func update_label_position(t: float) -> void:
 	var screen_size := DESIGN_SIZE
-	var label_size = $Label.size
+	var label_size := label.size
 
 	var y_progress := fmod(t * 0.12, 1.0)
 	var x_progress := fmod(t * 0.06, 1.0)
 
-	$Label.position = Vector2(
+	label.position = Vector2(
 		lerp(screen_size.x * 0.2, screen_size.x, x_progress),
 		lerp(screen_size.y, -label_size.y, y_progress)
 	)
